d76a0a17416be3b8c09c78992f1bf9070d41847a
Wen Tai-Chi <daleth90@gmail.com>
2021-01-30 00:49:44 +0800

    Simple movement logic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float defaultSpeed = 5.0f;
+
+    private float speed;
+
+    private void Start()
+    {
+        speed = defaultSpeed;
+    }
+
+    private void Update()
+    {
+        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void Move(float x, float y)
+    {
+        if (Mathf.Approximately(x, 0f) && Mathf.Approximately(y, 0f))
+        {
+            return;
+        }
+
+        float degree = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+        Vector2 direction = new Vector2(1f, 0f).Rotate(degree);
+        transform.localPosition = transform.localPosition + new Vector3(direction.x, direction.y, 0f) * speed * Time.deltaTime;
+        transform.localEulerAngles = new Vector3(0f, 0f, degree);
+    }
+}
---
+++ Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions
+{
+    public static Vector2 Rotate(this Vector2 vector, float degrees)
+    {
+        float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        float tx = vector.x;
+        float ty = vector.y;
+        vector.x = (cos * tx) - (sin * ty);
+        vector.y = (sin * tx) + (cos * ty);
+
+        return vector;
+    }
+}
