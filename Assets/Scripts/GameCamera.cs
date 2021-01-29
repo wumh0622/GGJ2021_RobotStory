59f114a5a3c4cb50b95ac168349c52ecaada241f
Wen Tai-Chi <daleth90@gmail.com>
2021-01-30 01:14:16 +0800

    Move Camera

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.01f, 1f)]
+    private float moveTime = 0.2f;
+
+    public void MoveTo(Vector2 position)
+    {
+        StopAllCoroutines();
+        StartCoroutine(MoveProcess(position));
+    }
+
+    private IEnumerator MoveProcess(Vector2 position)
+    {
+        Vector3 targetPosition = new Vector3(position.x, position.y, transform.position.z);
+
+        float t = 0f;
+        Vector3 originPosition = transform.position;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / moveTime;
+            transform.position = Vector3.Slerp(originPosition, targetPosition, t);
+            yield return null;
+        }
+    }
+}
